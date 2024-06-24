c37f1dff6a6506f1e455082cc9737420e9ece091
Dobson <bdobson@ic.ac.uk>
2024-06-24 13:34:50 +0100

    Update prepare_subselect_batch.py
    fix merge

--- src/prepare_subselect_batch.py
+++ src/prepare_subselect_batch.py
@@ -36,9 +36,6 @@ hpc_config['base_dir'] = hpc_address
 hpc_config['address_overrides']['precipitation'] = \
     '/rds/general/user/bdobson/home/swmmanywhere_paper/tests/test_data/storm.dat'
 del hpc_config['address_overrides']['national_building']
-hpc_config['api_keys'] = \
-    '/rds/general/user/bdobson/home/swmmanywhere_paper/tests/test_data/api_keys.yml'
-del hpc_config['address_overrides']['national_building']
 for cut, so in zip(cuts, subbasin_streamorder):
     subselect_cut(base_dir, base_project, cut, buffer = 1/500)
     cut_dir = base_dir / f'{base_project}_{cut}'
