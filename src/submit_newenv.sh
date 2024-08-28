c8ede60bbf26bf9e2d34ecd0cbd934c724cbf232
barneydobson <barnaby.dobson1@gmail.com>
2024-08-28 14:06:08 +0100

    Create new env each job

---
+++ src/submit_newenv.sh
@@ -0,0 +1,27 @@
+#PBS -lselect=1:ncpus=1:mem=20gb
+#PBS -lwalltime=01:00:00
+#PBS -J 0-2
+
+# subjob's index within the array
+
+## All subjobs run independently of one another
+
+
+# Load modules for any applications
+module load anaconda3/personal
+source activate swmmanywhere
+env_name="env_${PBS_ARRAY_INDEX}"
+python -m venv $env_name
+source deactivate
+source $env_name/bin/activate
+
+cd /rds/general/user/bdobson/home/swmmanywhere_paper
+pip install -e .
+
+# Change to the submission directory
+cd $PBS_O_WORKDIR
+
+# Run program, passing the index of this subjob within the array
+python experimenter.py --jobid=$PBS_ARRAY_INDEX --nproc=7000 --config_path=/rds/general/user/bdobson/ephemeral/swmmanywhere/cranbrook/cranbrook_hpc.yml
+
+deactivate rm -rf ,$env_name
