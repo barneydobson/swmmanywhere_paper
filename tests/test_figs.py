c70dd54cf235d5a04dd85080d2490cc02c9ea7a8
barneydobson <barnaby.dobson1@gmail.com>
2024-09-27 12:57:37 +0100

    pre-commit

--- tests/test_figs.py
+++ tests/test_figs.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
+
 from pathlib import Path
+
 from swmmanywhere_paper.plots import fig5
+
 
 def test_fig5():
     fig5.plot_fig5(Path(r'C:\Users\darne\Documents\data\swmmanywhere_paper_data'))
